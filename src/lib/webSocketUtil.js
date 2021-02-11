bfd8deee7c728a624948c8393a44909cef0b9b9b
pkkwilliam <pkkwilliam919@gmail.com>
2021-02-12 01:16:22 +0800

    first commit

---
+++ src/lib/webSocketUtil.js
@@ -0,0 +1,53 @@
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+
+export default class WebSocketUtil {
+  static _socket = null;
+  static _stompClient = null;
+  static _token = null;
+  static _subscribes = [];
+
+  constructor(requestUrl, token, subscribes) {
+    this._socket = new SockJS(requestUrl);
+    this._token = token;
+    this._subscribes = subscribes;
+    this._stompClient = Stomp.over(this.socket);
+    this.connect();
+  }
+
+  get socket() {
+    return this._socket;
+  }
+
+  get stompClient() {
+    return this._stompClient;
+  }
+
+  get subscribes() {
+    return this._subscribes;
+  }
+
+  get token() {
+    return this._token;
+  }
+
+  connect() {
+    console.log(this.stompClient);
+    this.stompClient.connect({}, this.onConnect, () => {
+      console.log("web socket error, please add overrride parent");
+    });
+  }
+
+  onConnect = () => {
+    console.log("web socket connected");
+    this.subscribes.forEach((subscribe) =>
+      this.stompClient.subscribe(subscribe.channel, (response) =>
+        subscribe.onMessageReceived(JSON.parse(response))
+      )
+    );
+  };
+
+  sendMessage(messageUrl, content) {
+    this.stompClient.send(messageUrl, {}, JSON.stringify(content));
+  }
+}
